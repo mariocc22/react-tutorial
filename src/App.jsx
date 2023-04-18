ad8378cefb571c94d5ba8f33cd4338fa04980f11
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-04-17 23:43:55 -0700

    adding tailwind to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import AdoptedPetContext from "./AdoptedPetContext";
 
 // this options are basically "how long do you want me to cache things?"
@@ -21,20 +19,29 @@ const App = () => {
   const adoptedPet = useState(null);
 
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        {/* this way the adopted pet is available in everything inside the bracket */}
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <div
+      className="m-0 p-0"
+      style={{
+        background: "url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          {/* this way the adopted pet is available in everything inside the bracket */}
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200 " to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -21,8 +21,9 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12 ">
       <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg "
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -41,7 +42,13 @@ const SearchParams = () => {
         ) : null}
         <label htmlFor="location">
           Location
-          <input name="location" id="location" placeholder="Location" />
+          <input
+            type="text"
+            name="location"
+            id="location"
+            placeholder="Location"
+            className="search-input"
+          />
         </label>
         <label htmlFor="animal">
           Animal
@@ -49,6 +56,7 @@ const SearchParams = () => {
             name="animal"
             id="animal"
             value={animal}
+            className="search-input"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -61,14 +69,21 @@ const SearchParams = () => {
         </label>
         <label htmlFor="breed">
           Breed
-          <select name="breed" id="breed" disabled={breeds.length === 0}>
+          <select
+            className="search-input grayed-out-disabled"
+            name="breed"
+            id="breed"
+            disabled={breeds.length === 0}
+          >
             <option key="default">Unknown</option>
             {breeds.map((breed) => (
               <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <button className="rounded border-none bg-orange-500 px-6 py-2 text-white transition-opacity hover:opacity-50">
+          Submit
+        </button>
       </form>
       {console.log("this is breed list", breeds, status)}
 
